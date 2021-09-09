14e58b47ce5fc561844c226755461d70f50cb153
AariyJP <aariyjp@aariy.net>
2021-09-09 21:17:19 +0900

    YouTubeApp

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-const { app, BrowserWindow, Menu, MenuItem, clipboard, Tray, nativeImage } = electron;
+const { app, BrowserWindow, Menu, MenuItem, clipboard, Tray, nativeImage, session } = electron;
 const rightMenu = require('electron-context-menu');
 
 let win = null;
@@ -13,15 +13,13 @@ app.on("ready", () =>
   win.loadURL(`https://www.youtube.com`);
   var webContents = win.webContents;
   tray = new Tray(nativeImage.createFromPath('youtube.png'));
-  
   const contextMenu = Menu.buildFromTemplate([
     { label: 'YouTubeApp', type: 'normal', enabled: false },
     { type: 'separator', enabled: false },
     { label: '終了', type: 'normal', click: () => { app.quit(); } }
   ]);
   tray.setContextMenu(contextMenu);
-
-    const menu = new Menu();
+  const menu = new Menu();
   menu.append(new MenuItem(
     {
     label: 'YouTubeApp',
@@ -73,11 +71,11 @@ app.on("ready", () =>
 
   webContents.on('page-favicon-updated', () =>
   {
-    webContents.insertCSS('* { font-family: system-ui!important; } ::-webkit-scrollbar { display:none; } #container.ytd-masthead, header { -webkit-app-region: drag!important; -webkit-user-select: none!important; } yt-icon-button, yt-icon, #container.ytd-searchbox, ytd-topbar-menu-button-renderer, a, ytcp-icon-button, ytcp-button, input { -webkit-app-region: no-drag!important; } html:not(.style-scope)[dark], :not(.style-scope)[dark] { --yt-spec-brand-background-primary: rgba(50, 0, 0, 0.9)!important } #items > ytd-compact-link-renderer:nth-child(3), #buttons > ytd-topbar-menu-button-renderer:nth-child(1), #items > ytd-compact-link-renderer:nth-child(5), #items > ytd-compact-link-renderer:nth-child(3) { display: none!important;}');
+    webContents.insertCSS('* { font-family: system-ui!important; } ::-webkit-scrollbar { display:none; } #container.ytd-masthead, header { -webkit-app-region: drag!important; -webkit-user-select: none!important; } yt-icon-button, yt-icon, #container.ytd-searchbox, ytd-topbar-menu-button-renderer, a, ytcp-icon-button, ytcp-button, input { -webkit-app-region: no-drag!important; } html:not(.style-scope)[dark], :not(.style-scope)[dark] { --yt-spec-brand-background-primary: rgba(50, 0, 0, 0.9)!important; } html:not(.style-scope) { --yt-spec-brand-background-primary: rgba(255, 200, 200, 0.9)!important; } #items > ytd-compact-link-renderer:nth-child(3), #buttons > ytd-topbar-menu-button-renderer:nth-child(1), #items > ytd-compact-link-renderer:nth-child(5), #items > ytd-compact-link-renderer:nth-child(3) { display: none!important;}');
     if(process.platform == "darwin")
       webContents.insertCSS('#start.ytd-masthead { margin-left: 60px; }');
     else
-      webContents.insertCSS('#end.ytd-masthead { margin-right: 120px; }');
+      webContents.insertCSS('#end.ytd-masthead { margin-right: 130px; }');
   });
 
   win.on("closed", () =>
